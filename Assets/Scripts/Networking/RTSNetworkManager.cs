eeee12a4f51f4171c8a6c907f56f0f979b5d473a
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-12-04 22:42:31 -0700

    basic spawn single unit over network

---
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSNetworkManager : NetworkManager
+{
+    [SerializeField] private GameObject unitSpawnerPrefab = null;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate(
+            unitSpawnerPrefab,
+            conn.identity.transform.position,
+            conn.identity.transform.rotation);
+
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
+
+}
