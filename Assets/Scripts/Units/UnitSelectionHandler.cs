1b65646dcbca3e99fa63264d2ff7a0a24bb784ee
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-12-07 07:48:39 -0700

    multi unit selection

--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,13 @@ using UnityEngine.InputSystem;
 
 public class UnitSelectionHandler : MonoBehaviour
 {
+    [SerializeField] private RectTransform unitSelectionArea = null;
+
     [SerializeField] private LayerMask layerMask = new LayerMask();
 
+    private Vector2 startPosition;
+
+    private RTSPlayer player;
     private Camera mainCamera;
 
     public List<Unit> SelectedUnits { get; } = new List<Unit>();
@@ -19,7 +25,28 @@ public class UnitSelectionHandler : MonoBehaviour
 
     private void Update()
     {
+        if (player == null)
+        {
+            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        }
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            StartSelectionArea();
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            ClearSelectionArea();
+        }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+            UpdateSelectionArea();
+        }
+    }
+
+    private void StartSelectionArea()
+    {
+        if (!Keyboard.current.leftShiftKey.isPressed)
         {
             foreach (Unit selectedUnit in SelectedUnits)
             {
@@ -28,27 +55,70 @@ public class UnitSelectionHandler : MonoBehaviour
 
             SelectedUnits.Clear();
         }
-        else if (Mouse.current.leftButton.wasReleasedThisFrame)
-        {
-            ClearSelectionArea();
-        }
+
+        
+
+        unitSelectionArea.gameObject.SetActive(true);
+
+        startPosition = Mouse.current.position.ReadValue();
+
+        UpdateSelectionArea();
+    }
+
+    private void UpdateSelectionArea()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        float areaWidth = mousePosition.x - startPosition.x;
+        float areaHeight = mousePosition.y - startPosition.y;
+
+        unitSelectionArea.sizeDelta = new Vector2(Mathf.Abs(areaWidth), Mathf.Abs(areaHeight));
+        unitSelectionArea.anchoredPosition = startPosition + new Vector2(areaWidth / 2, areaHeight / 2);
     }
 
     private void ClearSelectionArea()
     {
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        unitSelectionArea.gameObject.SetActive(false);
 
-        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
-
-        if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
-
-        if (!unit.hasAuthority) { return; }
-
-        SelectedUnits.Add(unit);
-
-        foreach(Unit selectedUnit in SelectedUnits)
+        if(unitSelectionArea.sizeDelta.magnitude == 0)
         {
-            selectedUnit.Select();
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
+
+            if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
+
+            if (!unit.hasAuthority) { return; }
+
+            SelectedUnits.Add(unit);
+
+            foreach(Unit selectedUnit in SelectedUnits)
+            {
+                selectedUnit.Select();
+            }
+
+        return;
+
         }
+
+        Vector2 min = unitSelectionArea.anchoredPosition - (unitSelectionArea.sizeDelta / 2);
+        Vector2 max = unitSelectionArea.anchoredPosition + (unitSelectionArea.sizeDelta / 2);
+
+        foreach(Unit unit in player.GetMyUnits())
+        {
+            if(SelectedUnits.Contains(unit)) { continue; }
+
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(unit.transform.position);
+
+            if (screenPosition.x > min.x && 
+                screenPosition.x < max.x && 
+                screenPosition.y > min.y && 
+                screenPosition.y < max.y)
+            {
+                SelectedUnits.Add(unit);
+                unit.Select();
+            }
+        }
+
     }
 }
