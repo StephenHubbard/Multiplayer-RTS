cb6f32eae34232aadf0e3b1cde0a62d5672a8a71
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-12-10 08:43:29 -0700

    unit projectile dmg

--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -8,6 +8,7 @@ public class UnitProjectile : NetworkBehaviour
     [SerializeField] private Rigidbody rb = null;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
+    [SerializeField] private int damageToDeal = 20;
 
     void Start()
     {
@@ -17,6 +18,22 @@ public class UnitProjectile : NetworkBehaviour
     public override void OnStartServer()
     {
         Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) { return; }
+        }
+
+        if (other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damageToDeal);
+        }
+
+        DestroySelf();
     }
 
     [Server]
